4f53cb1d2f04b053f2f60672c26021aebb68b80a
pradeep-selva <pradeepselvaraju@gmail.com>
2021-06-11 05:43:26 +0530

    compilation script complete

--- contract/compile.ts
+++ contract/compile.ts
@@ -2,16 +2,46 @@ import path = require("path");
 import solc from "solc";
 import fs = require("fs-extra");
 
-const buildPath = path.resolve(__dirname, "build");
-const contractFiles = ["Campaign.sol", "Factory.sol"];
+const buildPath = path.resolve("./", "build");
+const contractFiles = ["Campaign.sol", "Factory.sol", "Types.sol"];
+
+fs.removeSync(buildPath);
 
 const contractConfig = contractFiles.map((contract) => {
   const _path = path.resolve(__dirname, "src", contract);
 
   return {
     fileName: contract,
-    source: fs.readFileSync(_path, "utf-8")
+    content: fs.readFileSync(_path, "utf-8")
   };
 });
 
-console.log(contractConfig);
+const input = {
+  language: "Solidity",
+  sources: {},
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"]
+      }
+    }
+  }
+};
+
+contractConfig.forEach(({ fileName, content }) => {
+  (input.sources as any)[fileName] = {
+    content
+  };
+});
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+fs.ensureDirSync(buildPath);
+
+Object.keys(output?.contracts).forEach((contractName) => {
+  const contract = contractName.split(".")[0];
+  fs.outputJSONSync(
+    path.resolve(buildPath, `${contract}.json`),
+    output.contracts?.[contractName][contract]
+  );
+});
