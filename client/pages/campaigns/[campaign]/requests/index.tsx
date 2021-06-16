2190af54302fd12976e3bb6398acd2bcdca9b57c
pradeep-selva <pradeepselvaraju@gmail.com>
2021-06-16 21:59:06 +0530

    request page ui setup

--- client/pages/campaigns/[campaign]/requests/index.tsx
+++ client/pages/campaigns/[campaign]/requests/index.tsx
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
-import { Campaign } from "../../../../instances";
+import Link from "next/link";
+import { Container, Button } from "semantic-ui-react";
 import { getCampaignRequests } from "../../../../utils";
+import styles from "../../../../styles/Pages.module.css";
+import homeStyles from "../../../../styles/Home.module.css";
+import { Layout } from "../../../../components";
+import RouteNames from "../../../../routes";
 
 interface IProps {
   requests: Array<any>;
+  address: string;
 }
 
 class Requests extends Component<IProps, {}> {
   static async getInitialProps(context) {
-    const requests = await getCampaignRequests(context.query.campaign);
-    return { requests };
+    const address = context.query.campaign;
+    const requests = await getCampaignRequests(address);
+    return { requests, address };
   }
   render() {
-    console.log(this.props.requests);
-    return <div></div>;
+    const { requests, address } = this.props;
+
+    return (
+      <main className={homeStyles.main}>
+        <Layout>
+          <Container>
+            <Link
+              href={RouteNames.campaignById.absolute}
+              as={RouteNames.campaignById.as(address)}
+            >
+              <Button
+                icon={"arrow alternate circle left"}
+                floated={"left"}
+                size={"massive"}
+                primary
+                circular
+              />
+            </Link>
+            <div className={styles.centerContainer}>
+              <h1>
+                <u>Requests</u>
+              </h1>
+              <h3
+                className={styles.greyText}
+                style={{ marginTop: "-0.5rem" }}
+              >{`<{ ${address} }>`}</h3>
+            </div>
+          </Container>
+        </Layout>
+      </main>
+    );
   }
 }
 
