b4c71bf924f3082cbc7051c0143262152491c50a
pradeep-selva <pradeepselvaraju@gmail.com>
2021-06-27 13:25:51 +0530

    minor change

--- client/instances/factory.ts
+++ client/instances/factory.ts
@@ -5,4 +5,3 @@ import { FACTORY_ADDRESS } from "../config";
 const instance = new web3.eth.Contract(Factory.abi as any, FACTORY_ADDRESS);
 
 export default instance;
-// 0x7bb08511de45b13c08daf8efa4c0ba20c4eaa2cc;
